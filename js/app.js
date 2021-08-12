abbba639f889f12ff5bbea558ab3dca3ec34416d
holy-crusader <holy.tanukis@gmail.com>
2021-08-12 18:34:26 +0200

    Add project card

---
+++ js/app.js
@@ -0,0 +1,76 @@
+const data = {
+    "projects": {
+        "Mochi": {
+            "title": "Mochi",
+            "description": "Discord codded in python with a Dashboard.",
+            "tags": ["mochi", "discord"],
+            "multi-project": true,
+            "sub-projects": {
+                "Mochi DiscordBot": {
+                    "title": "Mochi",
+                    "description": "Mochi is an open source Discord bot developed in python.",
+                    "tags": ["mochi", "discord", "python"],
+                    "link": {
+                        "text": "Invite Mochi",
+                        "uri": "https://discord.com/api/oauth2/authorize?client_id=693456698299383829&permissions=8&scope=bot"
+                    },
+                    "github": "https://github.com/holy-tanuki/Mochi-DiscordBot"
+                },
+                "Mochi Dashboard": {
+                    "title": "Mochi Dashboard",
+                    "tags": ["mochi", "discord", "javascript"],
+                    "description": "Mochi dashboard is the open source dashboard for Mochi.",
+                    "link": {
+                        "text": "Mochi Dashboard",
+                        "uri": "https://z1269196b-z643fcf81-gtw.qovery.io/"
+                    },
+                    "github": "https://github.com/holy-tanuki/Mochi-Dashboard"
+                }
+            }
+        },
+        "Twitter": {
+            "title": "Twitter",
+            "description": "Clone of Twitter.",
+            "tags": ["javascript", "twitter"]
+        },
+        "Ubunty": {
+            "title": "Ubunty",
+            "description": "Linux distro based on Ubuntu. Ubunty fix some problems of Ubuntu.",
+            "tags": ["linux", "ubunty"]
+        },
+        "Test": {
+            "title": "Test",
+            "description": "Nothing.",
+            "tags": []
+        }
+    }
+}
+
+
+const urlParams = new URLSearchParams(window.location.search)
+
+const tag = urlParams.get('tag')
+console.log(tag)
+let project = []
+
+for (item in data.projects) {
+    if (tag == null || data.projects[item].tags.includes(tag)) {
+        project[project.length] = data.projects[item]
+    }
+}
+
+project = JSON.stringify(project)
+project = JSON.parse(project)
+
+console.log(project)
+
+const App = {
+    data() {
+        return {
+            projects: project,
+            tag: urlParams.get('tag')
+        }
+    }
+}
+    
+Vue.createApp(App).mount('#main');
