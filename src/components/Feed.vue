5b51265b9fddcd7001d3f546fd7738ad5d244bf3
Holy Crusader <holy.tanukis@gmail.com>
2023-02-04 01:15:52 +0100

    Feed update

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,21 +1,55 @@
 <script setup>
 import { ref } from 'vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Post from "./Post.vue"
 
-var posts = ref(3)
+library.add(faChevronLeft, faChevronRight)
+
+var postCount = ref(3)
+var postList = ref([
+    {id: 1, image_url: "https://picsum.photos/1000/600?random=1", title: "Sample"},
+    {id: 2, image_url: "https://picsum.photos/1000?random=2", title: "Sample"},
+    {id: 3, image_url: "https://picsum.photos/1000?random=3", title: "Sample"},
+    {id: 4, image_url: "https://picsum.photos/1000?random=4", title: "Sample"},
+    {id: 5, image_url: "https://picsum.photos/1000?random=5", title: "Sample"},
+    {id: 6, image_url: "https://picsum.photos/1000?random=6", title: "Sample"},
+    {id: 7, image_url: "https://picsum.photos/1000?random=7", title: "Sample"},
+    {id: 8, image_url: "https://picsum.photos/1000?random=8", title: "Sample"},
+])
 var btnShowMore = ref(true)
+var postFocus = ref("")
 
 const toggleShowMoreFeed = () => {
     if (btnShowMore.value == true) {
-        posts.value = 8;
+        postCount.value = 8;
         btnShowMore.value = false;
         $(".feed-button").text("Show less");
     } else {
-        posts.value = 3;
+        postCount.value = 3;
         btnShowMore.value = true;
         $(".feed-button").text("Show more");
     }
 }
+
+const getPosts = () => {
+    return postList.value.slice(0, postCount.value);
+}
+
+const fullscreenPost = (postId) => {
+    let post = postList.value.find(post => post.id == postId);
+    postFocus.value = post.image_url;
+    $("body").addClass("no-scroll");
+    $(".fullscreen-post").addClass("active");
+}
+
+const leaveFullscreenPost = () => {
+    postFocus.value = "";
+    $("body").removeClass("no-scroll");
+    $(".fullscreen-post").removeClass("active");
+}
+
 
 </script>
 
@@ -23,9 +57,22 @@ const toggleShowMoreFeed = () => {
   <div id="feed" class="section">
       <div class="container">
         <h1 class="title">Feed</h1>
+        <div class="fullscreen-post" @click="leaveFullscreenPost">
+            <img :src="postFocus" alt="" srcset="">
+            <div class="nav-overlay">
+                <div class="nav-overlay-container">
+                    <div class="nav-overlay-left nav-overlay-btn-container">
+                        <font-awesome-icon class="btn-nav-overlay" icon="fa-solid fa-chevron-left" />
+                    </div>
+                    <div class="nav-overlay-right nav-overlay-btn-container">
+                        <font-awesome-icon class="btn-nav-overlay" icon="fa-solid fa-chevron-right" />
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="post-handler">
             <div class="post-container">
-                <Post v-for="i in posts" :image_url="'https://picsum.photos/1000?random=' + i" title="Sample" />
+                <Post @click="fullscreenPost(id=post.id)" v-for="post in getPosts()" :image_url="post.image_url" :title="post.title" />
             </div>
         </div>
         <div class="toggle-feed">
@@ -61,6 +108,68 @@ const toggleShowMoreFeed = () => {
         justify-content: center;
         align-items: center;
     }
+
+    .fullscreen-post {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #fff;
+        z-index: 999;
+    }
+
+    .fullscreen-post.active {
+        display: flex !important;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .fullscreen-post img {
+        max-width: 80%; max-height: 80%;
+        object-fit: contain;
+        max-width: 50%;
+        object-fit: cover;
+    }
+
+    .nav-overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+    .nav-overlay-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+    .nav-overlay-btn-container {
+        width: 50px;
+        height: 100px;
+        display: flex;
+        align-items: center;
+        margin: auto 20px;
+
+    }
+    .nav-overlay-left {
+        justify-content: flex-start;
+    }
+
+    .nav-overlay-right {
+        justify-content: flex-end;
+    }
+
+    .btn-nav-overlay {
+        color: #000;
+        width: 100%; height: 100%;
+    }
+
+
 
     @media screen and (max-width: 1250px) {
         .post-container {
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -7,6 +7,8 @@
             <h2 class="title">Contact</h2>
             <h3 class="subtitle">Want to talk to me ?</h3>
         </div>
+        <div class="container">
+            <img src="MarmotFallout.png" alt="marmot fallout">
+        </div>
     </div>
-    <div class="section"></div>
 </template>
