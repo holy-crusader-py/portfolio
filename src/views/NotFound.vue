819d6eef0e8b7c70102031249d0898d690d12739
Holy Crusader <holy.tanukis@gmail.com>
2023-02-07 01:26:08 +0100

    Put a game in the 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>
-        
+    <div class="section">
+        <div class="container game-container">
+            <iframe id="game" src="https://itch.io/embed-upload/6840692?color=000" allowfullscreen="" frameborder=""><a href="https://holy-tanuki.itch.io/elder-killer-space-shooter">Play Elder Killer [Pico-8] on itch.io</a></iframe>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.game-container {
+    text-align: center;
+}
+#game {
+    border: 5px solid #000;
+    aspect-ratio: 640 / 660;
+    max-width: 640px; width: 100%;
+    height: auto;
+}
+</style>
