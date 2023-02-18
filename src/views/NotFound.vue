a6a7da6aaa198d2cd770c8da2dc3f89e2d7b6b25
Holy Crusader <holy.tanukis@gmail.com>
2023-02-19 00:56:35 +0100

    maybe ?

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="section">
         <div class="container game-container">
-            <iframe id="game" src="https://itch.io/embed-upload/7373593?color=333333" frameborder="0">
+            <iframe src="https://itch.io/embed-upload/7373593?color=333333" width="1152" height="668" frameborder="0">
                 <a href="https://holy-tanuki.itch.io/mrhandy-the-game">Play Mr.handy The Game on itch.io</a>
             </iframe>
         </div>
@@ -14,8 +14,8 @@
 }
 #game {
     border: 5px solid #000;
-    aspect-ratio: 1152 / 668;
-    max-width: 1000px; width: 100%;
+    aspect-ratio: 640 / 660;
+    max-width: 640px; width: 100%;
     height: auto;
 }
 </style>
